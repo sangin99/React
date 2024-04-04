fef1edf205ca0b9890f567886e13996795c26cd1
heesanggod <sangin583@gmail.com>
2024-04-04 10:36:28 +0900

    같은 내용의 함수에서 다른 내용은 변수지정하기

--- basic/src/component/example/InputBox.tsx
+++ basic/src/component/example/InputBox.tsx
@@ -5,25 +5,37 @@
 
 export function IdInputBox () {
 
+    //? 서로 다른 내용들을 '변수'로 지정한다
+    const label = '아이디';
+    const type = 'text';
+    const placeholder = '아이디를 입력해주세요';
+    // ========================================
+
     return (
         <div className="input-box">
-                            <div className="input-label label"> 아이디</div>
+                            <div className="input-label label">{label}</div>
                             <div className="input-content-box">
-                                <input id="id" className="input" type="text" placeholder="아이디를 입력해주세요." />
+                                <input className="input" type={type} placeholder={placeholder} />
                             </div>
                             <div className="input-message"></div>
                         </div>
     )
 }
 
+    //? 서로 다른 내용들을 '변수'로 지정한다
+    const label = '비밀번호';
+    const type = 'password';
+    const placeholder = '비밀번호를 입력해주세요';
+    // ========================================
+
 export function PasswordInputBox () {
     return (
         <div className="input-box">
-                            <div className="input-label label">비밀번호</div>
+                            <div className="input-label label">{label}</div>
                             <div className="input-content-box">
-                                <input id="password" className="input" type="password" placeholder="비밀번호를 입력해주세요." />
+                                <input className="input" type={type} placeholder={placeholder} />
                             </div>
-                            <div id="sign-in-message" className="input-message error"></div>
+                            <div className="input-message"></div>
                         </div>
     )
 }
