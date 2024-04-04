3cf74d71b3f4b92d226345e6624d4222777b97b6
heesanggod <sangin583@gmail.com>
2024-04-04 12:37:43 +0900

    Properties _ 예제

---
+++ basic/src/component_manage/example/InputBox.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export function InputBox2 () {
+
+    const label = '이메일';
+    const type = 'text';
+    const placeholder = '이메일 주소를 입력해주세요.';
+
+  return (
+    <div className="input-box">
+                            <div className="input-label label">{label}</div>
+                            <div className="input-content-box">
+                                <input className="input" type={type} placeholder={placeholder} />
+                                <div className="input-disable-button">이메일 인증</div>
+                            </div>
+                            <div className="input-message"></div>
+                        </div>
+  )
+}
+
+
+
+
+interface Props {
+    label: string;
+    type: 'text' | 'password';
+    placeholder: string;
+    buttonTitle: string;
+}
+
+export default function InputBox({ label, type, placeholder, buttonTitle }: Props) {
+    return (
+        <div className="input-box">
+            <div className="input-label label">{label}</div>
+            <div className="input-content-box">
+                <input
+                    className="input"
+                    type={type}
+                    placeholder={placeholder}
+                />
+                <div className="input-disable-button"> {buttonTitle}
+                </div>
+            </div>
+            <div className="input-message"></div>
+        </div>
+    );
+}
---
+++ basic/src/component_manage/example/SignInContainer.tsx
@@ -0,0 +1,19 @@
+// SignInContainer.tsx
+
+// SignInContainer 함수형 컴포넌트를 생성
+// 해당 컴포넌트는 InputBox 컴포넌트를 2개 포함
+// SignInContainer 컴포넌트를 기본 값으로 내보내기 
+
+import React from 'react'
+import InputBox from './InputBox'
+
+import './style.css';
+import './common.css';
+
+export default function SignInContainer() {
+  return (
+    <>
+    {/* <InputBox label="이메일" type="text" placeholder = "이메일을 입력해주세요" /> */}
+    </>
+  );
+}
