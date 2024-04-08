2ac298a38435eea13e98b82d296b9784a9ac9b1d
heesanggod <sangin583@gmail.com>
2024-04-08 12:23:00 +0900

    State

--- basic/src/interaction/StateComponent.tsx
+++ basic/src/interaction/StateComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 // 상태(status) :
 // - 각각의 컴포넌트를 가지는 데이터
@@ -19,6 +19,9 @@ export default function StateComponent() {
     const [count, setCount] = useState<number>(0);
     // let total : number = 0;
     const [total, setTotal] = useState<number>(0);
+    // let favorites: string[] = [];
+    const [favorites, setFavorites] = useState<string[]>(['사과']);
+    const [favorite, setFavorite] = useState<string>('');
 
     const onCountAddHandler = () => {
         // setCount(count + 1);
@@ -48,8 +51,30 @@ export default function StateComponent() {
 
         // 변경된 상태를 사용하고 싶을 때 해결방법, 임시 변수를 사용하여 간접 사용
         const tmp = count + 1;   //간접적으로 작업해서 그 값을 넣어줌
-        setCount(count + 1);
-        setTotal(total + count);
+        setCount(tmp + 1);
+        setTotal(total + tmp);
+    };
+
+    // Input 요소의 값을 가져오고자 할 땐 onChange 이벤트의 .target.value 로 가져옴
+    const onInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        // 가져온 .target.value 값을 상태에 지정
+        setFavorite(event.target.value);
+    };
+
+    const onAddListHandler = () => {
+        // 상태가 배열 혹은 객체이면 각각에 대해 요소를 추가, 변경이 일어나도 상태가 변경된 것으로 인식하지 않음(실제 주소 변경 X)
+        // 상태가 변경된 것으로 인식하게 하고 싶으면 새로운 배열 혹은 객체를 생성하여 상태를 변경해야 함 (일반적으로 복사해서 변경)
+
+        // //? 복사를 하여 추가하는 방법
+        // favorites.push(favorite);  //추가
+        // const newFavorites = favorites.map(item => item);   // 배열은 주소를 가지고, 주소를 변경한다.
+        // setFavorites(newFavorites);  //변경/출력
+
+        setFavorites([...favorites, favorite]);
+        // setFavorites(['사과', favorite]);
+        // setFavorites(['사과', '바나나' , favorite]);
+        // setFavorites(['사과', '바나나' , '포도' , favorite]);
+        setFavorite('');
     };
 
     return (
@@ -57,6 +82,16 @@ export default function StateComponent() {
         <button onClick={onCountAddHandler}>+</button>
         <h1>{count}</h1>
         <h1>{total}</h1>
+
+        <hr/>
+        
+        {/* input 이 만약 상태를 변경하다면 value 로 그 상태를 지정해야 불일치가 발생하지 않음 */}
+        <input onChange={onInputHandler} value={favorite                                                           } />
+        <button onClick={onAddListHandler}>추가</button>
+        <h4>{favorite}</h4>
+        <ul>
+            {favorites.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
     </>
     );
 }
