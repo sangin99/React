25ce4f238df0c764ec712f57d9423f6d662d081c
heesanggod <sangin583@gmail.com>
2024-04-16 12:58:24 +0900

    코드 중복 작업

--- basic/src/hook/CustomHook.tsx
+++ basic/src/hook/CustomHook.tsx
@@ -2,10 +2,11 @@ import React, { useEffect, useRef, useState } from 'react'
 
 // Custom Hook : 
 // - 리엑트의 훅 함수는 반드시 함수형 컴포넌트의 코드 블럭에서만 호출가능
+// - 함수명을 use 로 시작하게 작성하여 커스텀 훅 함수를 만들 수 있음
+// - 커스텀 훅 함수에는 다른 훅 함수를 포함할 수 있음
 
+function usePagination(totalList: string[]) {
 
-export default function CustomHook() {
-    
     const [page, setPage] = useState<number>(1);
     const [section, setSection] = useState<number>(1);
     const [boardList, setBoardList] = useState<string[]>([]);
@@ -59,7 +60,7 @@ export default function CustomHook() {
     };
 
     useEffect(() => {
-        const boardList = BOARD_LIST;
+        const boardList = totalList;
         setBoardList(boardList);
         totalPageCount.current = Math.floor((boardList.length -1) / COUNT_PER_PAGE) + 1;
         totalSectionCount.current = Math.floor((boardList.length -1) / COUNT_PER_PAGE * COUNT_PER_SECTION) + 1;
@@ -77,11 +78,18 @@ export default function CustomHook() {
         changeViewPageList();
     }, [section]);
 
+    return {
+        page, setPage, viewList, viewPageList, setPreviousSection, setNextSection
+    }
+}
+
+export default function CustomHook() {
+    
+    const { page, setPage, viewList, viewPageList, setPreviousSection, setNextSection } = usePagination(BOARD_LIST);
+
     return (
     <div>
-        {totalPageCount.current}
-        {totalSectionCount.current}
-    {viewList.map((item, index) => <h3 key={index}>{item}</h3>)}
+        {viewList.map((item, index) => <h3 key={index}>{item}</h3>)}
         <div>
             <span style={{ display: 'inline-block', margin: '4px' }} onClick={setPreviousSection}>이전</span>
                 {viewPageList.map((item, index) => <span key={index} 
@@ -108,12 +116,10 @@ const BOARD_LIST = [
     '게시물 56','게시물 57','게시물 58','게시물 59','게시물 60',
     '게시물 61','게시물 62','게시물 63','게시물 64','게시물 65',
     '게시물 66','게시물 67','게시물 68','게시물 69','게시물 70',
-
     '게시물 71','게시물 72','게시물 73','게시물 74','게시물 75',
     '게시물 76','게시물 77','게시물 78','게시물 79','게시물 80',
     '게시물 81','게시물 82','게시물 83','게시물 84','게시물 85',
     '게시물 86','게시물 87','게시물 88','게시물 89','게시물 90',
-
     '게시물 91','게시물 92','게시물 93','게시물 94','게시물 95',
     '게시물 96','게시물 97',
 ];
