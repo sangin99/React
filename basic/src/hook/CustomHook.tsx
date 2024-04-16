638d4c93081da2c6a58987c4a722eb5701ed182d
heesanggod <sangin583@gmail.com>
2024-04-16 12:39:08 +0900

    게시물 페이지 이동

--- basic/src/hook/CustomHook.tsx
+++ basic/src/hook/CustomHook.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 // Custom Hook : 
 // - 리엑트의 훅 함수는 반드시 함수형 컴포넌트의 코드 블럭에서만 호출가능
@@ -11,33 +11,34 @@ export default function CustomHook() {
     const [viewList, setViewList] = useState<string[]>([]); 
     const [viewPageList, setViewPageList] = useState<number[]>([]); 
 
+    const totalPageCount = useRef<number>(1);
+    const totalSectionCount = useRef<number>(1);
+
     const COUNT_PER_PAGE = 5;
     const COUNT_PER_SECTION = 10;
-
-    let totalPageCount = 0;
-    let totalSectionCount = 0;
     
-    const setNextPage = () => {
-
-    }
-
-    const setPreviousPage = () => {
-
-    }
-
     const setNextSection = () => {
+        if (section === totalSectionCount.current) return;
+        setSection(section + 1);
 
+        const page = (section + 1) * COUNT_PER_SECTION + 1;
+        setPage(page);
     }
 
     const setPreviousSection = () => {
+        if (section === 1) return;
+        setSection(section - 1);
 
+        const page = (section - 1) * COUNT_PER_SECTION;
+        setPage(page);
     }
 
     useEffect(() => {
         const boardList = BOARD_LIST;
         setBoardList(BOARD_LIST);
-        totalPageCount = Math.floor((boardList.length -1) / COUNT_PER_PAGE) + 1;
-        totalSectionCount = Math.floor((boardList.length -1) / COUNT_PER_PAGE * COUNT_PER_SECTION) + 1;
+        totalPageCount.current = Math.floor((boardList.length -1) / COUNT_PER_PAGE) + 1;
+        totalSectionCount.current = Math.floor((boardList.length -1) / COUNT_PER_PAGE * COUNT_PER_SECTION) + 1;
+
 
         const viewList = [];
         const currentPageStart = COUNT_PER_PAGE * (page - 1);
@@ -54,20 +55,49 @@ export default function CustomHook() {
         const currentSectionEnd = COUNT_PER_SECTION * section;
 
         for (let page = currentSectionStart; page <= currentSectionEnd; page++) {
-            if (page > totalPageCount) break;
+            if (page > totalPageCount.current) break;
             viewPageList.push(page);
         }
         setViewPageList(viewPageList);
 
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        if (!boardList.length) return;
+        const viewList = [];
+        const currentPageStart = COUNT_PER_PAGE * (page - 1);
+        const currentPageEnd = COUNT_PER_PAGE * page - 1;
+
+        for (let index = currentPageStart; index <= currentPageEnd; index++) {
+            if (currentPageEnd >= boardList.length) break;
+            viewList.push(boardList[index]);
+        }
+        setViewList(viewList);
+    }, [page]);
+
+    useEffect(() => {
+        const viewPageList = [];
+        const currentSectionStart = COUNT_PER_SECTION * section - (COUNT_PER_SECTION - 1);
+        const currentSectionEnd = COUNT_PER_SECTION * section;
+
+        for (let page = currentSectionStart; page <= currentSectionEnd; page++) {
+            if (page > totalPageCount.current) break;
+            viewPageList.push(page);
+        }
+        setViewPageList(viewPageList);
+    }, [section])
 
     return (
     <div>
+        {totalPageCount.current}
+        {totalSectionCount.current}
     {viewList.map((item, index) => <h3 key={index}>{item}</h3>)}
         <div>
             <span style={{ display: 'inline-block', margin: '4px' }}>이전</span>
-            {viewPageList.map((item, index) => <span key={index} style={{ display: 'inline-block', margin: '4px', fontWeight: item === page ? 900 : 400 }}>{item}</span>)}
-            <span style={{ display: 'inline-block', margin: '4px' }}>이후</span>
+                {viewPageList.map((item, index) => <span key={index} 
+                style={{ display: 'inline-block', margin: '4px', fontWeight: item === page ? 900 : 400 }} 
+                onClick={()=>setPage(item)}>{item}</span>)}
+            <span style={{ display: 'inline-block', margin: '4px' }} onClick={setNextSection}>이후</span>
         </div>
     </div>
     );
@@ -81,6 +111,17 @@ const BOARD_LIST = [
     '게시물 21','게시물 22','게시물 23','게시물 24','게시물 25',
     '게시물 26','게시물 27','게시물 28','게시물 29','게시물 30',
     '게시물 31','게시물 32','게시물 33','게시물 34','게시물 35',
+    '게시물 36','게시물 37','게시물 38','게시물 39','게시물 40',
+    '게시물 41','게시물 42','게시물 43','게시물 44','게시물 45',
+    '게시물 36','게시물 37','게시물 38','게시물 39','게시물 40',
+    '게시물 36','게시물 37','게시물 38','게시물 39','게시물 40',
+    '게시물 36','게시물 37','게시물 38','게시물 39','게시물 40',
+    '게시물 36','게시물 37','게시물 38','게시물 39','게시물 40',
+    '게시물 36','게시물 37','게시물 38','게시물 39','게시물 40',
+    '게시물 36','게시물 37','게시물 38','게시물 39','게시물 40',
+    '게시물 36','게시물 37','게시물 38','게시물 39','게시물 40',
+    '게시물 36','게시물 37','게시물 38','게시물 39','게시물 40',
+    '게시물 36','게시물 37','게시물 38','게시물 39','게시물 40',
     '게시물 36','게시물 37','게시물 38','게시물 39','게시물 40',
     '게시물 41','게시물 42',
 ]
