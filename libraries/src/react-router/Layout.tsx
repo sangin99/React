842ab16fa6a85aa77b65b5cc581041ea606247d5
heesanggod <sangin583@gmail.com>
2024-04-18 10:13:04 +0900

    useLocation 경로표시

--- libraries/src/react-router/Layout.tsx
+++ libraries/src/react-router/Layout.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router';
+import React, { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router';
 
 export default function Layout() {
     // <Outlet /> :
     // - 부모 <Route>에 해당 컴포넌트가 element 로 등록되었을 때 자식 <Route>의 element 가 해당 위치에 표시되도록 하는 컴포넌트
+
+    // useLocation :
+    // - 현재 URL 및 위치에 location 객체를 반환
+    // - pathname : 현재 위치 URL
+    const { pathname } = useLocation();
+
   return (
     <>
-        <h2 style={{color: 'red'}}>Router</h2>
+        <h2 style={{color: 'red'}}>Router: { pathname }</h2>
         <Outlet />
     </>
   )
