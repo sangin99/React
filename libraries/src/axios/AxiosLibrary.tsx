f9abb4357d35213c0937ab2c282e5c1ff052075a
heesanggod <sangin583@gmail.com>
2024-04-18 10:44:52 +0900

    axios

---
+++ libraries/src/axios/AxiosLibrary.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// axios 패키지(라이브러리) :
+// - javascript 에서 사용하는 HTTP 클라이언트 라이브러리
+// - 웹 브라우저, Node.js 기반의 환경에서 동작함
+// - Ajax, Fetch API 보다 간편함
+// - axios api 는 기본적으로 Promise 로 작업을 수행
+
+// - npm i axios
+export default function AxiosLibrary() {
+  return (
+    <div>AxiosLibrary</div>
+  )
+}
